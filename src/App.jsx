1cbdd7b119cd3ccbef38ff670d661712fa7c3ac4
Mateo <mateobetancurb@gmail.com>
2023-07-06 22:15:35 -0500

    using about component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { Header } from "./components/Header";
+import { About } from "./components/About";
 
 function App() {
 	return (
 		<>
 			<div style={{ backgroundColor: "#242424" }}>
 				<Header />
+			</div>
+			<div>
+				<About />
 			</div>
 		</>
 	);
