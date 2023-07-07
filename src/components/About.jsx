956189e76db700dd076602c2e61966213cfdfb07
Mateo <mateobetancurb@gmail.com>
2023-07-06 22:15:19 -0500

    about component

---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+import "../styles/about.css";
+
+const About = () => {
+	return (
+		<>
+			<main className="container about">
+				<h2>¿Quién soy?</h2>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
+					repudiandae, dolor impedit delectus perferendis accusantium.
+				</p>
+			</main>
+		</>
+	);
+};
+
+export { About };
