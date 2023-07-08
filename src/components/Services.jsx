842aa38142680fd110b2278f4605624ec32c8bf3
Mateo <mateobetancurb@gmail.com>
2023-07-08 18:28:11 -0500

    services component

---
+++ src/components/Services.jsx
@@ -0,0 +1,56 @@
+import "../styles/index.css";
+import "../styles/services.css";
+
+const Services = () => {
+	return (
+		<section className="container services">
+			<h2 className="services__h2">¿Qué puedo hacer por ti?</h2>
+			<div className="grid__services">
+				<div>
+					<h3>Web Development</h3>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+						voluptatum, voluptate, quibusdam, quia voluptas quod dolorum
+					</p>
+				</div>
+				<div>
+					<h3>Web Development</h3>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+						voluptatum, voluptate, quibusdam, quia voluptas quod dolorum
+					</p>
+				</div>
+				<div>
+					<h3>Web Development</h3>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+						voluptatum, voluptate, quibusdam, quia voluptas quod dolorum
+					</p>
+				</div>
+				<div>
+					<h3>Web Development</h3>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+						voluptatum, voluptate, quibusdam, quia voluptas quod dolorum
+					</p>
+				</div>
+				<div>
+					<h3>Web Development</h3>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+						voluptatum, voluptate, quibusdam, quia voluptas quod dolorum
+					</p>
+				</div>
+				<div>
+					<h3>Web Development</h3>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+						voluptatum, voluptate, quibusdam, quia voluptas quod dolorum
+					</p>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export { Services };
