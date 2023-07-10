ca79930a0456205c09a4c14cdad130622ec1f3b3
Mateo <mateobetancurb@gmail.com>
2023-07-10 11:29:28 -0500

    header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,14 +8,17 @@ function Header() {
 	return (
 		<>
 			<header className="header container">
-				<img src={Logo} alt="Logo de sitio web" className="header__logo" />
+				<picture>
+					<img src={Logo} alt="Logo de sitio web" className="header__logo" />
+				</picture>
 				<nav className="header__nav">
-					<ul className="header__ul">
+					<div className="header__ul">
 						<a
 							className="header__social--media"
 							href="https://twitter.com/MateoBetancurB"
 							target="_blank"
 							rel="noreferrer"
+							aria-label="Twitter"
 						>
 							<i className="fab fa-twitter mt-4"></i>
 						</a>
@@ -24,6 +27,7 @@ function Header() {
 							href="https://www.instagram.com/mateobetancurb/"
 							target="_blank"
 							rel="noreferrer"
+							aria-label="Instagram"
 						>
 							<i className="fab fa-instagram mt-4"></i>
 						</a>
@@ -32,6 +36,7 @@ function Header() {
 							href="https://github.com/mateobetancurb"
 							target="_blank"
 							rel="noreferrer"
+							aria-label="GitHub"
 						>
 							<i className="fab fa-github mt-4"></i>
 						</a>
@@ -40,14 +45,21 @@ function Header() {
 							href="https://www.linkedin.com/in/mateobetancurb"
 							target="_blank"
 							rel="noreferrer"
+							aria-label="LinkedIn"
 						>
 							<i className="fab fa-linkedin mt-4"></i>
 						</a>
-					</ul>
+					</div>
 				</nav>
 			</header>
 			<div className="container header__text">
-				<img src={Photo} alt="Fotografía de Mateo Betancur" className="header__img" />
+				<picture>
+					<img
+						src={Photo}
+						alt="Fotografía de Mateo Betancur"
+						className="header__img"
+					/>
+				</picture>
 				<div>
 					<h1 className="header__text--h1">
 						Hola! Soy Mateo... programador de software
