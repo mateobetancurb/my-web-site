ebb716afbe9fb19f955c86d72cce115b1a6af4fe
Mateo <mateobetancurb@gmail.com>
2023-07-11 13:33:40 -0500

    config netlfy

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,38 +6,41 @@ import "../styles/form.css";
 
 const Form = () => {
 	const [disableBtn, setDisableBtn] = useState(false);
-	const [showOtherService, setShowOtherService] = useState(false);
 	const [showAlert, setShowAlert] = useState(false);
-	const [name, setName] = useState("");
-	const [email, setEmail] = useState("");
-	const [service, setService] = useState("");
-	const [otherService, setOtherService] = useState("");
-	const [description, setDescription] = useState("");
+	const [formData, setFormData] = useState({
+		name: "",
+		email: "",
+		service: "",
+		description: "",
+	});
 
-	//TODO: message when user finish correctly the form
-	//TODO: clear form state
+	const handleChange = (e) => {
+		setFormData({
+			...formData,
+			[e.target.name]: e.target.value,
+		});
+	};
 
 	useEffect(() => {
 		if (
-			name.trim() !== "" &&
-			email.trim() !== "" &&
-			service.trim() !== "" &&
-			description.trim() !== ""
+			formData.name.trim() !== "" &&
+			formData.email.trim() !== "" &&
+			formData.service.trim() !== "" &&
+			formData.description.trim() !== ""
 		) {
 			setDisableBtn(false);
 		}
-	}, [name, email, service, description]);
+	}, [formData.name, formData.email, formData.service, formData.description]);
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		if (
-			name.trim() === "" ||
-			email.trim() === "" ||
-			service.trim() === "" ||
-			description.trim() === "" ||
-			(service === "Otro" && otherService.trim() === "")
+			formData.name.trim() === "" ||
+			formData.email.trim() === "" ||
+			formData.service.trim() === "" ||
+			formData.description.trim() === "" ||
+			(formData.service === "Otro" && formData.otherService.trim() === "")
 		) {
-			console.log("no se puede enviar");
 			setShowAlert(true);
 			setDisableBtn(true);
 			return;
@@ -45,40 +48,64 @@ const Form = () => {
 		setShowAlert(false);
 	};
 
+	const encode = (data) => {
+		return Object.keys(data)
+			.map(
+				(key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+			)
+			.join("&");
+	};
+
+	const sendData = async () => {
+		try {
+			const response = await fetch("/", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/x-www-form-urlencoded",
+				},
+				body: encode({ "form-name": "contact-form", ...formData }),
+			});
+			console.log(response);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
 	return (
 		<>
 			<form
 				onSubmit={handleSubmit}
 				name="contact"
-				method="POST"
 				data-netlify="true"
 				className="container form"
 			>
-				<input type="hidden" name="form-name" value="contact" />
 				<h2 className="form__h2">Contáctame</h2>
 				<p className="form__p">Comencemos a crear eso que tienes en mente...</p>
 				{showAlert && <Alert message="Todos los campos son obligatorios" />}
 				<input
-					onChange={(e) => setName(e.target.value)}
 					type="text"
 					name="name"
+					id="name"
+					value={formData.name}
+					onChange={handleChange}
 					placeholder="Escribe tu nombre"
 					className="form__input"
 				/>
 				<input
-					onChange={(e) => setEmail(e.target.value)}
 					type="email"
 					name="email"
+					id="email"
+					value={formData.email}
+					onChange={handleChange}
 					placeholder="Escribe tu correo electrónico"
 					className="form__input"
 				/>
 				<select
-					onChange={(e) => {
-						setService(e.target.value);
-						setShowOtherService(e.target.value === "Otro");
-					}}
-					className="form__input round form__select"
 					name="service"
+					id="service"
+					value={formData.service}
+					onChange={handleChange}
+					className="form__input round form__select"
 				>
 					<option value="">-- Selecciona un servicio --</option>
 					{services.map((service) => (
@@ -91,25 +118,18 @@ const Form = () => {
 						</option>
 					))}
 				</select>
-				{showOtherService && (
-					<input
-						onChange={(e) => setOtherService(e.target.value)}
-						type="text"
-						name="otherService"
-						placeholder="Escribe el tipo de servicio"
-						className="form__input"
-					/>
-				)}
 				<textarea
-					onChange={(e) => setDescription(e.target.value)}
+					name="description"
+					value={formData.description}
+					onChange={handleChange}
 					cols="30"
 					rows="5"
-					name="description"
 					placeholder="Escribe una breve descripción del servicio seleccionado"
 					className="form__input"
 				></textarea>
 				<button
 					type="submit"
+					onClick={sendData}
 					className={disableBtn ? "form__button--disable" : "form__button"}
 					disabled={disableBtn}
 				>
@@ -121,3 +141,6 @@ const Form = () => {
 };
 
 export { Form };
+
+//TODO: message when user finish correctly the form
+//TODO: clear form state
