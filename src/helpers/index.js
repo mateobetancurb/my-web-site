9f42affe9ce0ea2991f7feb854028ba76aadb415
Mateo <mateobetancurb@gmail.com>
2023-07-11 12:13:54 -0500

    helpers folder

---
+++ src/helpers/index.js
@@ -0,0 +1,9 @@
+export const services = [
+	{ id: 1, name: "Sitios web" },
+	{ id: 2, name: "Aplicaciones web" },
+	{ id: 3, name: "Asesorías" },
+	{ id: 4, name: "Optimización" },
+	{ id: 5, name: "Diseño" },
+	{ id: 6, name: "WordPress" },
+	{ id: 7, name: "Otro" },
+];
