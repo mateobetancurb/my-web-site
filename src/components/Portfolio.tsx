a3a819a74796a224e266137de2e80c89f5f86af9
Mateo <71zaps-memento@icloud.com>
2025-04-06 15:46:03 -0500

    feat: add Portfolio component to showcase recent projects

---
+++ src/components/Portfolio.tsx
@@ -0,0 +1,41 @@
+import { Card } from "./ui/Card";
+
+const Portfolio = () => {
+	return (
+		<>
+			<section
+				id="portfolio"
+				className="w-full bg-black py-12 md:py-24 lg:py-32"
+			>
+				<div className="container mx-auto px-4 md:px-6">
+					<div className="flex flex-col items-center justify-center space-y-4 text-center">
+						<div className="space-y-2">
+							<h2 className="text-3xl text- font-bold tracking-tighter text-white sm:text-5xl">
+								Mi portafolio
+							</h2>
+							<p className="max-w-[900px] text-gray-300 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+								Explora una selección de mis proyectos recientes que muestran
+								mis habilidades y experiencia
+							</p>
+						</div>
+					</div>
+					<div className="mx-auto grid max-w-5xl grid-cols-1 gap-6 py-12 md:grid-cols-2 lg:grid-cols-3">
+						{[1, 2, 3, 4, 5, 6].map((item) => (
+							<Card
+								key={item}
+								image="..."
+								title="Proyecto cool"
+								subtitle="App web"
+								description="	Una aplicación web moderna construida con React y Next.js, con diseño
+					      responsivo y una experiencia de usuario fluida"
+								buttonText="Ver más"
+							/>
+						))}
+					</div>
+				</div>
+			</section>
+		</>
+	);
+};
+
+export { Portfolio };
