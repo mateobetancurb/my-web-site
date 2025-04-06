8972df1ef1ede9c35b2eb8d1a1c428d905a13a8e
Mateo <71zaps-memento@icloud.com>
2025-04-06 13:14:29 -0500

    feat: integrate TanStack Router for routing management

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import {
+	createRouter as createTanStackRouter,
+	RouterProvider,
+} from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+
+const router = createTanStackRouter({
+	routeTree,
+	defaultPreload: "intent",
+});
+
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof router;
+	}
+}
+
 function App() {
-	return (
-		<>
-			<h1>hello world!</h1>
-		</>
-	);
+	return <RouterProvider router={router} />;
 }
 
 export { App };
