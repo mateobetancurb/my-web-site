81b54ac148b070eaf9c8fd9a3b2ce24e34f5d3e8
Mateo <71zaps-memento@icloud.com>
2025-04-06 15:45:56 -0500

    feat: add Portfolio component to main route for enhanced project showcase

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { Header } from "../components/Header";
 import { Hero } from "../components/Hero";
 import { About } from "../components/About";
 import { Experience } from "../components/Experience";
+import { Portfolio } from "../components/Portfolio";
 
 export const Route = createFileRoute("/")({
 	component: RouteComponent,
@@ -16,6 +17,7 @@ function RouteComponent() {
 				<Hero />
 				<About />
 				<Experience />
+				<Portfolio />
 			</main>
 		</>
 	);
