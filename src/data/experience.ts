4e13b4c4869353fb3ac076c1e073e6a5c6f4ab20
Mateo <71zaps-memento@icloud.com>
2025-04-15 20:12:26 -0500

    feat: add experience data for showcasing professional background

---
+++ src/data/experience.ts
@@ -0,0 +1,22 @@
+import { ExperienceProps } from "../types/experience";
+
+export const experienceData: ExperienceProps[] = [
+	{
+		id: 1,
+		title: "Desarrollador Web Frontend",
+		company: "Freelance",
+		dates: "2021 - Actualidad",
+		description:
+			"Desarrollo de aplicaciones web y sitios responsivos utilizando tecnologías modernas como React, Next.js y Tailwind CSS.",
+		skills: ["JavaScript", "React", "Next.js", "Tailwind CSS"],
+	},
+	{
+		id: 2,
+		title: "Desarrollador Web Junior",
+		company: "Digital Creations Agency",
+		dates: "2019 - 2021",
+		description:
+			"Comencé mi carrera construyendo sitios web responsivos y soluciones de comercio electrónico. Adquirí valiosa experiencia en prácticas modernas de desarrollo web y comunicación con clientes.",
+		skills: ["HTML", "CSS", "JavaScript", "WordPress"],
+	},
+];
