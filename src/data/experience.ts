75a58a899a7c6eb2c57d0f104ca953cdf5f31434
Mateo <71zaps-memento@icloud.com>
2025-04-15 20:38:19 -0500

    feat: update experience data for improved detail and accuracy

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -8,15 +8,34 @@ export const experienceData: ExperienceProps[] = [
 		dates: "2021 - Actualidad",
 		description:
 			"Desarrollo de aplicaciones web y sitios responsivos utilizando tecnologías modernas como React, Next.js y Tailwind CSS.",
+		highlights: [],
 		skills: ["JavaScript", "React", "Next.js", "Tailwind CSS"],
 	},
 	{
 		id: 2,
 		title: "Desarrollador Web Junior",
-		company: "Digital Creations Agency",
-		dates: "2019 - 2021",
+		company: "Vinix Code",
+		dates: "2020 - 2023",
 		description:
-			"Comencé mi carrera construyendo sitios web responsivos y soluciones de comercio electrónico. Adquirí valiosa experiencia en prácticas modernas de desarrollo web y comunicación con clientes.",
-		skills: ["HTML", "CSS", "JavaScript", "WordPress"],
+			"Desempeñé un rol importante en el desarrollo de soluciones digitales innovadoras, abarcando tanto el frontend como el backend. Mi enfoque principal fue adquirir experiencia en el desarrollo frontend, utilizando tecnologías modernas para crear interfaces atractivas y funcionales",
+		highlights: [
+			"Creé interfaces interactivas y atractivas utilizando JavaScript, Vue y React, contribuyendo a la experiencia de usuario",
+			"Participé en el diseño y construcción de APIs robustas y administré la lógica del servidor con Node.js y Express.js, asegurando la eficiencia y escalabilidad",
+			"Gestioné bases de datos MySQL y SQL para el almacenamiento y manipulación eficiente de datos",
+			"Participé activamente en el desarrollo fullstack, integrando mis habilidades en el frontend y backend para una solución completa",
+		],
+		skills: [
+			"HTML",
+			"CSS",
+			"JavaScript",
+			"TypeScript",
+			"Vue",
+			"React",
+			"Node.js",
+			"Express.js",
+			"MySQL",
+			"PostgreSQL",
+			"SQL",
+		],
 	},
 ];
