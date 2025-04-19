694d832d773d251005aa6fde3aa0d582e6748bc2
Mateo <71zaps-memento@icloud.com>
2025-04-19 14:34:57 -0500

    feat: add date prop to Card component and display it with an icon

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,6 +1,7 @@
 interface CardProps {
 	image: string;
 	title: string;
+	date: string;
 	subtitle?: string;
 	description: string;
 	categories?: string[];
@@ -19,6 +20,7 @@ interface CardProps {
 const Card = ({
 	image,
 	title,
+	date,
 	subtitle,
 	description,
 	categories,
@@ -45,7 +47,7 @@ const Card = ({
 					{categories?.map((category) => (
 						<span
 							key={category}
-							className="bg-[#F3F4F6] text-black  inline-block mb-3 text-[12px] rounded-full px-2 font-medium"
+							className="bg-[#F3F4F6] text-black  inline-block mb-3 text-[11px] rounded-full px-2 font-medium"
 						>
 							{category}
 						</span>
@@ -56,6 +58,25 @@ const Card = ({
 					<p className={`text-gray-400 text-sm ${styles.subtitle || ""}`}>
 						{subtitle}
 					</p>
+				</div>
+				<div className="flex items-center gap-2">
+					<svg
+						width="15"
+						height="15"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="#71717A"
+						strokeWidth="2"
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						className="lucide lucide-calendar-icon lucide-calendar"
+					>
+						<path d="M8 2v4" />
+						<path d="M16 2v4" />
+						<rect width="18" height="18" x="3" y="4" rx="2" />
+						<path d="M3 10h18" />
+					</svg>
+					<span className="text-[#71717A] text-sm">{date}</span>
 				</div>
 			</div>
 			<div className="px-6 pb-4 flex-grow">
