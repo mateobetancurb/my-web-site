3b927a866493428784908c5dd440db43f28b8865
Mateo <71zaps-memento@icloud.com>
2025-04-19 14:35:13 -0500

    feat: add BlogPostProps interface for blog post structure

---
+++ src/interfaces/blogPost.ts
@@ -0,0 +1,11 @@
+export interface BlogPostProps {
+	id: string;
+	title: string;
+	date: string;
+	author: string;
+	slug: string;
+	excerpt: string;
+	categories: string[];
+	image: string;
+	content: string;
+}
