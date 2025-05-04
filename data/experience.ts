c1c4a07dbe39814acb7a1d49f1c5d500b3586fdc
Mateo <71zaps-memento@icloud.com>
2025-05-04 13:33:32 -0500

    Add experience data for web developer roles

---
+++ data/experience.ts
@@ -0,0 +1,64 @@
+import type { ExperienceProps } from "../src/interfaces/experience";
+
+export const experienceData: ExperienceProps[] = [
+	{
+		id: 1,
+		title: "Desarrollador Web",
+		company: "BBD",
+		dates: "2023 - Actualidad",
+		description:
+			"Como Desarrollador mi enfoque está en la creación de interfaces web dinámicas y eficientes utilizando React.js, Next.js, integrando Node.js y AWS para potenciar ciertas funcionalidades y asegurar una infraestructura robusta. Destaco por optimizar el rendimiento, SEO y la experiencia del usuario, incluyendo la integración con servicios de terceros. Tengo experiencia en el desarrollo y mantenimiento de un e-commerce de alto rendimiento",
+		highlights: [
+			"Implemento interfaces dinámicas con React.js y Vue.js, creando componentes reutilizables para una mayor eficiencia. Utilizo Node.js para el desarrollo y optimización de ciertas funcionalidades de lado del backend",
+			"Optimizo el rendimiento y el SEO implementando SSR y SSG con Next.js y Nuxt.js, resultando en una reducción del tiempo de carga de las vistas internas de la app",
+			"Implemento integraciones exitosas con servicios de terceros, incluyendo BigCommerce, APIs de mapas y localización, mejorando la funcionalidad y experiencia del usuario",
+			"Soy parte clave en el desarrollo y mantenimiento de un e-commerce de alto rendimiento, con un alcance de más de 500,000 usuarios en México, asegurando su escalabilidad y funcionalidad continua",
+			"Implemento caching para reducir el tiempo de carga de la página y optimizo la infraestructura de la aplicación utilizando AWS",
+			"Desarrollo soluciones escalables y robustas para garantizar la eficiencia a largo plazo, aprovechando las capacidades de Node.js y AWS",
+		],
+		skills: [
+			"TypeScript",
+			"React",
+			"Next",
+			"Vue",
+			"Nuxt",
+			"Node",
+			"TailwindCSS",
+			"AWS",
+			"BigCommerce",
+			"PostgreSQL",
+			"Jest",
+			"Vitest",
+			"SASS",
+			"GraphQL",
+			"RESTful APIs",
+		],
+	},
+	{
+		id: 2,
+		title: "Desarrollador Web Junior",
+		company: "Vinix Code",
+		dates: "2020 - 2023",
+		description:
+			"Desempeñé un rol importante en el desarrollo de soluciones digitales innovadoras, abarcando tanto el frontend como el backend. Mi enfoque principal fue adquirir experiencia en el desarrollo frontend, utilizando tecnologías modernas para crear interfaces atractivas y funcionales",
+		highlights: [
+			"Creé interfaces interactivas y atractivas utilizando JavaScript, Vue y React, contribuyendo a la experiencia de usuario",
+			"Participé en el diseño y construcción de APIs robustas y administré la lógica del servidor con Node.js y Express.js, asegurando la eficiencia y escalabilidad",
+			"Gestioné bases de datos MySQL y SQL para el almacenamiento y manipulación eficiente de datos",
+			"Participé activamente en el desarrollo fullstack, integrando mis habilidades en el frontend y backend para una solución completa",
+		],
+		skills: [
+			"HTML",
+			"CSS",
+			"JavaScript",
+			"TypeScript",
+			"Vue",
+			"React",
+			"Node.js",
+			"Express.js",
+			"MySQL",
+			"PostgreSQL",
+			"SQL",
+		],
+	},
+];
